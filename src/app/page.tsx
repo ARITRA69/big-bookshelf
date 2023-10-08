aa6d903a9423b8114d1d952c3401b230c9d9694f
Pritam Gain <pritamjob2021@gmail.com>
2023-10-08 15:15:42 +0530

    fix:browse_now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,15 +14,16 @@ export default function Home() {
         >
           reading makes the world huge
         </h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-6 justify-between">
-          <div className="h-[50vh] bg-[#2a2c2e] w-full rounded-2xl text-[#f6f4f0] flex flex-col justify-between p-4">
-            <div className="flex justify-between gap-2">
-              <div className="flex flex-col gap-4 relative">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-6 justify-between ">
+          <div className="h-[50vh] bg-[#2a2c2e] w-full rounded-2xl text-[#f6f4f0] flex flex-col justify-between p-4 relative">
+            <div className="lg:flex lg:flex-row flex-col justify-between gap-2 relative">
+              <div className="flex flex-col gap-4">
                 <h1 className="uppercase text-[36px]">
                   find something to read
                 </h1>
                 <p>
                   Fancy something unusual and unpredictable? Funny or exciting?
+                  <br />
                   No problem. Check out the collections we have prepared for
                   you.
                 </p>
@@ -35,7 +36,8 @@ export default function Home() {
               className="object-cover rounded-2xl"
             />
             </div>
-            <button className="border border-[#f4ce47] text-[#f4ce47] uppercase py-3 px-4 rounded-xl w-max">
+            <button className="border border-[#f4ce47] text-[#f4ce47] uppercase py-3 px-4 rounded-xl w-max absolute
+             bottom-4">
               browse now
             </button>
           </div>
