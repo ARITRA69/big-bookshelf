d374e4de801790f0334716047aad7b9a7851685c
Pritam Gain <pritamjob2021@gmail.com>
2023-10-09 08:19:47 +0530

    added  hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import BestSeller from "@/components/BestSeller";
+import Hero from "@/components/Hero";
 import localFont from "next/font/local";
 import Image from "next/image";
 import Link from "next/link";
@@ -15,45 +16,9 @@ export default function Home() {
         >
           reading makes the world huge
         </h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-6 justify-between ">
-          <div className="h-[50vh] bg-[#2a2c2e] w-full rounded-2xl text-[#f6f4f0] flex flex-col justify-between p-4 relative">
-            <div className="lg:flex lg:flex-row flex-col justify-between gap-2 relative">
-              <div className="flex flex-col gap-4">
-                <h1 className="uppercase text-[36px]">
-                  find something to read
-                </h1>
-                <p>
-                  Fancy something unusual and unpredictable? Funny or exciting?
-                  <br />
-                  No problem. Check out the collections we have prepared for
-                  you.
-                </p>
-              </div>
-              <Image
-              src="/hero.png"
-              width={300}
-              height={300}
-              alt=""
-              className="object-cover rounded-2xl"
-            />
-            </div>
-            <button className="border border-[#f4ce47] text-[#f4ce47] uppercase py-3 px-4 rounded-xl w-max absolute
-             bottom-4">
-              browse now
-            </button>
-          </div>
-          <div className="h-[50vh] bg-[#f5e5c4] w-full rounded-2xl text-[#2a2c2e] flex justify-center items-center">
-            <Image
-              src="/hero.png"
-              width={500}
-              height={500}
-              alt=""
-              className="object-cover"
-            />
-          </div>
-        </div>
+        <Hero />
       </div>
-      <BestSeller />
+      {/* <BestSeller /> */}
     </main>
   );
 }
