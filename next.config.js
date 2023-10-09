287e1f5c1211913796eeaada0905dd40600fb590
swarnadeep <aritrasarkar2002@gmail.com>
2023-10-10 01:51:50 +0530

    all books page layout has done but not responsive

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["images.template.net", "images-na.ssl-images-amazon.com", "www.creativeparamita.com", "images-na.ssl-images-amazon.com", "www.theartworksinc.com", "api.entangledpublishing.com", "www.orbitbooks.net", "static-cse.canva.com"]
+        domains: ["images.template.net", "images-na.ssl-images-amazon.com", "www.creativeparamita.com", "images-na.ssl-images-amazon.com", "www.theartworksinc.com", "api.entangledpublishing.com", "www.orbitbooks.net", "static-cse.canva.com", "www.ingramspark.com"]
     }
 }
 
